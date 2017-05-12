187c6e0686f30ff2ad01223f264d6f3e9d67e13f
Dima <bazavluk.d.s@gmail.com>
2017-05-12 19:05:03 +0300

    #4-Make-switch

--- index.php
+++ index.php
@@ -2,9 +2,6 @@
 require_once ('controller/main_controller.php');
 
 $mainController = new MainController();
-<<<<<<< HEAD
-//$mainController->viewReturn();
-// var_dump($_POST);
 switch ($_POST) {
     case is_null($_POST):
         $mainController->viewReturn();
@@ -14,10 +11,7 @@ switch ($_POST) {
         return $mainController->subTypeAction();
     break;
 
-    case!is_null($_POST['lol']):
+    case!is_null(@$_POST['lol']):
         return $mainController->prodAction();
     break;
 }
-=======
-$mainController->viewReturn();
->>>>>>> master
--- subtype.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if (!empty($_POST['code'])) {
-    require_once ('controller/main_controller.php');
-    $subType = new mainController();
-    return $subType->subTypeAction();
-    exit;
-}
